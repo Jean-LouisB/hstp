b44a73f169545de6fe9e76002671dfca2cda383a
Fabrice Kopf <contact@fabricekopf.fr>
2023-09-14 17:27:08 +0200

    Commentaire

--- src/app/users/user-page/user-page.component.ts
+++ src/app/users/user-page/user-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
-import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/userModel';
 import { SessionState } from 'src/app/state/session/session.reducers';
 import { setUser } from 'src/app/state/session/session.actions';
@@ -14,28 +13,44 @@ import { ServerService } from 'src/app/services/serveur/server.service';
   styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent implements OnInit{
-  private userSubscription: Subscription | undefined;
+
+  /**
+   * user récupère le profil de l'utilisateur connecté
+   */
   user: User | null = null;
   constructor(
     private store: Store<{ session: SessionState }>,
     private router: Router,
-    private apiBDD: ServerService,
+    private apiBDD: ServerService, //Service local qui interroge le serveur et donc la BDD
   ){}
- 
+ /**
+  * this.getNameUserState() affecte un profil à l'attribut local.
+  * Si le store a été rafraichi, donc réinitialisé, user == null
+  * donc ngOnInit interroge le serveur pour récupérer les infos avec le token.
+  * Puis les réaffecte à l'attribut local et au store.
+  * 
+  */
   ngOnInit(): void {
     this.getNameUserState();
     if (this.user == null) {
-      this.apiBDD.getUserProfil().subscribe((data) => {
+        this.apiBDD.getUserProfil().subscribe((data) => {
         this.user = new User().deserialize(data)
-        //console.log(this.user);
         this.store.dispatch(setUser({ user: this.user }));
       })
     }
 
   }
 
+/**
+ * getNameUserState() est lancée par ngOnInit()
+ * Cette fonction récupère le profil de l'utilisateur dans le store.
+ * Si les droit du profil récupèré sont < 3 alors l'utilisateur n'a pas le droit d'être là.
+ * Il est redirigé vers sa page d'accueil.
+ * sinon le profil est récupéré sur l'attribut local.
+ * 
+ * */
   getNameUserState() {
-    this.userSubscription = this.store.pipe(select(state => state.session.userState))
+    this.store.pipe(select(state => state.session.userState))
       .subscribe((userData: { user: User | null }) => {
         if (userData.user.type<3) {
           this.router.navigate(['/accueil'])
