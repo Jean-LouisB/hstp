b73db706cfcf821b8ac93ab0e699859c7e0e1275
Fabrice Kopf <contact@fabricekopf.fr>
2023-09-14 17:43:57 +0200

    Commenatires

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -7,43 +7,72 @@ import { ServerService } from '../../services/serveur/server.service';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent implements OnInit{
+export class UserListComponent implements OnInit {
+  /**
+   * userList : contient une copie brute et complète  de la liste des utilisateurs et leur profil
+   */
   userList = [];
+  /**
+   * filtreAbsent est un booléen activé sur true par défaut pour masquer les absents. 
+   * titreBtnFiltre modififie le titre du bouton pour afficher/masquer les absents
+   */
   filtreAbsent = true;
   titreBtnFiltre = "Voir les absents";
+  /**
+   * filterByName récupère la saisie dans l'Input de recherche.
+   * Il est utilisé par la fonction de recherche par nom 'filtreLaListe'
+   */
   filterByName = "*";
-  filteredList=[];
+  /**
+   * filteredList contient la liste filtrée qui est affichée par le composant.
+   */
+  filteredList = [];
+  /**
+   * comme son nom l'indique, c'est l'attribut local qui est mis à jour avec le nombre de salariés présents
+   */
   nbSalaries = 0;
 
 
   constructor(
-    private apiBDD : ServerService,
-     ){}
-
-  ngOnInit(){
+    private apiBDD: ServerService,
+  ) { }
+  /**
+   * A l'initialisation, le composant récupère l'ensemble des profils, les trie par nom de famille,
+   * puis le met dans l'attribut local
+   * puis lance la fonction qui initialise la liste affichée 'filtreLaListe().
+   * Enfin lance la fonction qui compte les présents.
+   */
+  ngOnInit() {
     this.apiBDD.getAllUsers()
       .then(response => {
         const list = response.data;
-        list.sort((a, b)=>a.nom.localeCompare(b.nom))
+        list.sort((a, b) => a.nom.localeCompare(b.nom))
         this.userList = list;
         this.filtreLaListe()
       })
       .catch(error => {
-        console.log("Une erreur s'est produite : "+error);
+        console.log("Une erreur s'est produite : " + error);
       })
-      this.countEmployees()
+    this.countEmployees()
   }
-  handelOnFilter(){
+  /**
+   * Cette fonction bascule le booléen qui gère l'affichage des absents
+   * et change le nom du bouton.
+   */
+  handelOnFilter() {
     this.filtreAbsent = !this.filtreAbsent
     this.titreBtnFiltre = this.filtreAbsent === true ? "Voir les absents" : "Masquer les absents"
   }
 
-
+  /**
+   * Récupère l'ensemble des profils, les met dans l'attribut local userList
+   * puis lance la fonction de filtre.
+   */
   fetchUsers() {
     this.apiBDD.getAllUsers().then(
       (data) => {
         const list = data.data;
-        list.sort((a, b)=>a.nom.localeCompare(b.nom))
+        list.sort((a, b) => a.nom.localeCompare(b.nom))
         this.userList = list;
         this.filtreLaListe()
       },
@@ -52,32 +81,46 @@ export class UserListComponent implements OnInit{
       }
     );
   }
-  
-  filtreLaListe(){
-    if (this.filterByName === "*" || this.filterByName === "" ){
+  /**
+   * Cette fonction récupère l'attribut local contenant la chaine saisie par l'utilisateur.
+   * Puis dans la liste vérifie si cette chaine fait partie de l'ensemble nom+prenom
+   * Tout est mis en majuscule pour homogénéiser les casses.
+   */
+  filtreLaListe() {
+    if (this.filterByName === "*" || this.filterByName === "") {
       this.filteredList = this.userList;
-    }else{
-      this.filteredList = this.userList.filter((user)=>{
-      const userNameUpperCase = user.nom.toUpperCase()
-      const userFirstNameUpperCase = user.prenom.toUpperCase()
-      const nameSearched = this.filterByName.toUpperCase()
-      const nomPrenom = userNameUpperCase+userFirstNameUpperCase
-      return nomPrenom.includes(nameSearched) 
-    })
+    } else {
+      this.filteredList = this.userList.filter((user) => {
+        const userNameUpperCase = user.nom.toUpperCase()
+        const userFirstNameUpperCase = user.prenom.toUpperCase()
+        const nameSearched = this.filterByName.toUpperCase()
+        const nomPrenom = userNameUpperCase + userFirstNameUpperCase
+        return nomPrenom.includes(nameSearched)
+      })
     }
-   
-  }
 
-  handleOnSearch(event: Event){
+  }
+  /**
+   * 
+   * @param event Cet attribut est envoyer par l'évènement (Input) du champs de saisi.
+   * A chaque charactère saisi il se met à jour.
+   * Puis il initialise l'attribut local avec sa valeur.
+   * Enfin il filtre la liste affichée de façon dynamique.
+   */
+  handleOnSearch(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     this.filterByName = inputElement.value
     this.filtreLaListe();
   }
 
-  countEmployees(){
+  /**
+   * 
+   * @returns ...le nombre d'employés présent
+   */
+  countEmployees() {
     let count = 0;
-    this.userList.map((user)=>{
-      if(user.present == 1){
+    this.userList.map((user) => {
+      if (user.present == 1) {
         count++
       }
     })
