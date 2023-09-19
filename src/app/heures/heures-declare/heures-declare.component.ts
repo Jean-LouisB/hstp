bd589f70fffb89e3693d19ea3604585fd5dc1879
Fabrice Kopf <contact@fabricekopf.fr>
2023-09-19 10:10:21 +0200

    Transformation des heures Str en décimal et calcul du temps

--- src/app/heures/heures-declare/heures-declare.component.ts
+++ src/app/heures/heures-declare/heures-declare.component.ts
@@ -1,48 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
+import { Component, Input, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { Subscription } from 'rxjs';
-import { User } from 'src/app/models/userModel';
-import { ServerService } from 'src/app/services/serveur/server.service';
-import { setUser } from 'src/app/state/session/session.actions';
-import { SessionState } from 'src/app/state/session/session.reducers';
+
 
 @Component({
   selector: 'app-heures-declare',
   templateUrl: './heures-declare.component.html',
   styleUrls: ['./heures-declare.component.css']
 })
-export class HeuresDeclareComponent implements OnInit{
-  private userSubscription: Subscription | undefined;
-  user: User | null = null;
+export class HeuresDeclareComponent implements OnInit {
   date_debut: Date = null;
   date_fin: Date = null;
-  constructor( 
-    private apiBDD: ServerService,
-    private store: Store<{ session: SessionState }>,
+  dateEvenement: Date = null;
+  sens: string = null;
+  heureDebut: string = null;
+  heureFin: string = null;
+  commentaire: string = null;
+
+  constructor(
     private cookieService: CookieService,
-    ){}
+  ) { }
 
   ngOnInit(): void {
-    this.getNameUserState();
-    if (this.user == null) {
-      this.apiBDD.getUserProfil().subscribe((data) => {
-        this.user = new User().deserialize(data)
-        this.store.dispatch(setUser({ user: this.user }));
-      })
-    }
+    this.getDatesDeSaisie();
+  }
+
+  getDatesDeSaisie() {
     const bornesDeSaisie = this.cookieService.get('bornes');
     const bornesDeSaisieJson = JSON.parse(bornesDeSaisie);
-    //const date_minimum = formatDate(mesBornesJson['date_debut'])
     this.date_debut = new Date(bornesDeSaisieJson['date_debut']);
     this.date_fin = new Date(bornesDeSaisieJson['date_fin']);
   }
 
-  getNameUserState() {
-    this.userSubscription = this.store.pipe(select(state => state.session.userState))
-      .subscribe((userData: { user: User | null }) => {
-        this.user = userData.user;
-      });
-    }
-
+  getTemps(){
+    const heureDebutDec = this.getHeureStrToDec(this.heureDebut);
+    const heureFinDec = this.getHeureStrToDec(this.heureFin);
+    console.log(heureFinDec-heureDebutDec);
+    
+    return heureFinDec-heureDebutDec;
+  }
+  getHeureStrToDec(heure: string){
+    const heureMinuscule = heure.toLowerCase();
+    const heureSplited = heureMinuscule.split('h');
+    const heureValueDec = parseInt(heureSplited[0]);
+    const minutesValueDec = parseInt(heureSplited[1])/60;
+    return heureValueDec +minutesValueDec;
+  }
 }
--- src/app/heures/heures.component.ts
+++ src/app/heures/heures.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { CookieService } from 'ngx-cookie-service';
+import { Subscription } from 'rxjs';
+import { User } from '../models/userModel';
+import { ServerService } from '../services/serveur/server.service';
+import { setUser } from '../state/session/session.actions';
+import { SessionState } from '../state/session/session.reducers';
 
 @Component({
   selector: 'app-heures',
   templateUrl: './heures.component.html',
   styleUrls: ['./heures.component.css']
 })
-export class HeuresComponent {
+export class HeuresComponent implements OnInit {
+  //private userSubscription: Subscription | undefined;
+  user: User | null = null;
+  date_debut: Date = null;
+  date_fin: Date = null;
+  constructor(
+    private apiBDD: ServerService,
+    private store: Store<{ session: SessionState }>,
+    private cookieService: CookieService,
+  ) { }
 
+  ngOnInit(): void {
+    this.getNameUserState();
+    if (this.user == null) {
+      this.apiBDD.getUserProfil().subscribe((data) => {
+        this.user = new User().deserialize(data)
+        this.store.dispatch(setUser({ user: this.user }));
+      })
+    }
+
+  }
+  getNameUserState() {
+    this.store.pipe(select(state => state.session.userState))
+      .subscribe((userData: { user: User | null }) => {
+        this.user = userData.user;
+      });
+  }
 }
